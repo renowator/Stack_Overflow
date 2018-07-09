9a45dddbd9dc2f4f98afc2269b466741552c5f21
scottbot3000 <scott.carbonbased@gmail.com>
2018-07-09 16:05:01 -0700

    Added the vertical prototype page and began work on connecting it to the database images.

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const {Client} = require('pg');
+const { Client } = require('pg');
 
 const client = new Client({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: 'postgres://rzolioxhicdcbq:2dcdefed515615296c818c19a1bae98a6dac3962ac5de97c5e200deb80539b08@ec2-23-21-166-148.compute-1.amazonaws.com:5432/df31neji5vbebi',
   ssl: true,
 });
 
 client.connect();
 
-client.query('SELECT * FROM Posting;', (err, res) => {
-  if (err) throw err;
+client.query(`SELECT * FROM Posting WHERE Name ~~* '%T%'`, (err, res) => {
+
+  if (err) {
+    throw err;
+  }
   for (let row of res.rows) {
     console.log(JSON.stringify(row));
   }
+  client.end();
 });
+
+console.log("hello")
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -23,6 +29,7 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/vertical-prototype', (req, res) => res.render('pages/vertical-prototype'))
   .get('/about', (req, res) => res.render('pages/about'))
   .get('/about/ScottPenn', (req, res) => res.render('pages/aboutScott'))
   .get('/about/AnDao', (req, res) => res.render('pages/aboutAn'))
