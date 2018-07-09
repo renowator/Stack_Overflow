b2846627ab3cce793ef688465dca120ef644f944
scottbot3000 <scott.carbonbased@gmail.com>
2018-07-09 10:33:38 -0700

    Renamed index.js to app.js to reduce confuson. Added entries to database and began work connecting to main app

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const {Client} = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT * FROM Posting;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+});
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
+  .use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
