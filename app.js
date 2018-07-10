bc118ed9dfbab85bf36836a05bc9fa2a164d8d72
scottbot3000 <scott.carbonbased@gmail.com>
2018-07-09 21:32:44 -0700

    Added a search function that queries the database and displays the results in the vertical prototype page.

--- app.js
+++ app.js
@@ -3,25 +3,41 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 const { Client } = require('pg');
 
-const client = new Client({
+const database = new Client ({
   connectionString: 'postgres://rzolioxhicdcbq:2dcdefed515615296c818c19a1bae98a6dac3962ac5de97c5e200deb80539b08@ec2-23-21-166-148.compute-1.amazonaws.com:5432/df31neji5vbebi',
   ssl: true,
 });
 
-client.connect();
+database.connect();
 
-client.query(`SELECT * FROM Posting WHERE Name ~~* '%T%'`, (err, res) => {
+database.query(`SELECT * FROM Posting WHERE Name ~~* '%T%'`, (err, res) => {
 
   if (err) {
     throw err;
   }
   for (let row of res.rows) {
-    console.log(JSON.stringify(row));
+    //console.log(JSON.stringify(row));
   }
-  client.end();
 });
 
 console.log("hello")
+var results = 0;
+
+function search(req, res, next) {
+  var searchTerm = req.query.search;
+  
+  database.query('SELECT Image FROM Posting WHERE Name ~~* $1', ['%' + searchTerm + '%'], (error, result) => {
+    if (error) {
+      console.log(error)
+      throw error;
+    }
+
+    req.searchResult = result.rows.map(x => String(x.image));
+    req.searchTerm = searchTerm;
+    console.log(searchTerm)
+    next();
+  });
+}
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -29,7 +45,10 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/vertical-prototype', (req, res) => res.render('pages/vertical-prototype'))
+  .get('/vertical-prototype', search, (req, res) => {
+      searchResult = req.searchResult;
+      res.render('pages/vertical-prototype', {results : searchResult.length, searchTerm : req.searchTerm, searchResult: searchResult});
+    })
   .get('/about', (req, res) => res.render('pages/about'))
   .get('/about/ScottPenn', (req, res) => res.render('pages/aboutScott'))
   .get('/about/AnDao', (req, res) => res.render('pages/aboutAn'))
